40d462958e6e6889c7e75c120f8f743409930dab
YarikineZ <38810461+YarikineZ@users.noreply.github.com>
2023-05-20 16:15:35 +0300

    App Draft with Firebase reading problem

---
+++ messenger/lib/main.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:messenger/services/database_servise.dart';
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  //Чтобы Flutter успел инициализировать fluter channel перед Firebase
+
+  await Firebase.initializeApp(
+      name: "flutter_messenger", //Название проекта в Firebase
+      options: DefaultFirebaseOptions.currentPlatform);
+
+  final databaseService = DatabaseService();
+  databaseService.getData();
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    var scaffold = Scaffold(
+        appBar: AppBar(
+          title: Text("Chat"),
+        ),
+        bottomSheet: Padding(
+          //padding: MediaQuery.of(context).viewInsets,
+          padding: EdgeInsets.symmetric(horizontal: 20.0),
+          child: Row(
+            children: [
+              Expanded(
+                child: TextField(
+                  style: TextStyle(fontSize: 16.0),
+                  decoration: InputDecoration(
+                      labelText: "Message",
+                      border: OutlineInputBorder(),
+                      floatingLabelBehavior: FloatingLabelBehavior.never),
+                ),
+              ),
+              IconButton(onPressed: () {}, icon: const Icon(Icons.send))
+            ],
+          ),
+        ));
+    return scaffold;
+  }
+}
---
+++ messenger/lib/services/database_servise.dart
@@ -0,0 +1,17 @@
+﻿import 'package:firebase_database/firebase_database.dart';
+
+class DatabaseService {
+  //FirebaseDatabase database = FirebaseDatabase.instance;
+
+  Future getData() async {
+    DatabaseReference ref = FirebaseDatabase.instance.ref();
+
+    final snapshot = await ref.child('message').get();
+
+    if (snapshot.exists) {
+      print(snapshot.value);
+    } else {
+      print('No data available.');
+    }
+  }
+}

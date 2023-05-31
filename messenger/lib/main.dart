86d87bdfd9f047e76595add5150b765365c18e97
YarikineZ <38810461+YarikineZ@users.noreply.github.com>
2023-06-01 01:32:32 +0300

    Перенес на Freezed, вынес виджеты в отдельные классы

--- messenger/lib/main.dart
+++ messenger/lib/main.dart
@@ -6,7 +6,7 @@ import 'models/message.dart';
 import 'package:timeago/timeago.dart' as timeago;
 import 'package:string_to_hex/string_to_hex.dart';
 
-void main() async {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
@@ -35,14 +35,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  final TextEditingController _controller = TextEditingController();
   final _database = DatabaseService();
-
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -52,85 +45,122 @@ class _MyHomePageState extends State<MyHomePage> {
         appBar: AppBar(
           title: const Text("Chat"),
         ),
-        body: StreamBuilder(
-            stream: _database.abRef.onValue,
-            builder: (context, snapshot) {
-              List<Message> messageList = [];
-
-              if (snapshot.hasData &&
-                  snapshot.data != null &&
-                  (snapshot.data!).snapshot.value != null) {
-                final firebaseMessages = Map<dynamic, dynamic>.from(
-                    (snapshot.data!).snapshot.value as Map<dynamic, dynamic>);
-                firebaseMessages.forEach((key, value) {
-                  final currentMessage = Map<String, dynamic>.from(value);
-                  messageList.add(Message.fromMap(currentMessage));
-                });
-                messageList.sort((a, b) => a.timestamp.compareTo(b.timestamp));
-
-                return Padding(
-                    padding: const EdgeInsets.all(16.0),
-                    child: ListView.builder(
-                        reverse: false,
-                        itemCount: messageList.length,
-                        itemBuilder: (context, index) {
-                          return Padding(
-                            padding: const EdgeInsets.only(top: 16),
-                            child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Row(children: [
-                                    Text(messageList[index].userId,
-                                        style: TextStyle(
-                                            fontWeight: FontWeight.bold,
-                                            color: Color(StringToHex.toColor(
-                                                messageList[index].userId)))),
-                                    const SizedBox(width: 6),
-                                    Text(
-                                      timeago.format(
-                                          DateTime.fromMillisecondsSinceEpoch(
-                                              messageList[index].timestamp)),
-                                      style: const TextStyle(
-                                          fontWeight: FontWeight.w400,
-                                          color: Colors.black38,
-                                          fontSize: 13.0),
-                                    )
-                                  ]),
-                                  const SizedBox(
-                                    height: 8,
-                                  ),
-                                  Text(messageList[index].text,
-                                      style: const TextStyle(fontSize: 16.0))
-                                ]),
-                          );
-                        }));
-              } else {
-                return const Center(child: Text("No messages"));
-              }
-            }),
-        bottomSheet: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 20.0),
-          child: Row(
-            children: [
-              Expanded(
-                child: TextField(
-                  controller: _controller,
-                  style: const TextStyle(fontSize: 16.0),
-                  decoration: const InputDecoration(
-                      labelText: "Message",
-                      border: OutlineInputBorder(),
-                      floatingLabelBehavior: FloatingLabelBehavior.never),
-                ),
-              ),
-              IconButton(
-                  onPressed: () {
-                    _database.sendMessage(_controller.text);
-                    _controller.text = "";
-                  },
-                  icon: const Icon(Icons.send))
-            ],
-          ),
-        ));
+        body: MessagesList(database: _database),
+        bottomSheet: BottomSheet(database: _database));
     return scaffold;
+  }
+}
+
+class MessagesList extends StatelessWidget {
+  const MessagesList({
+    super.key,
+    required DatabaseService database,
+  }) : _database = database;
+
+  final DatabaseService _database;
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder(
+        stream: _database.abRef.onValue,
+        builder: (context, snapshot) {
+          List<Message> messageList = [];
+
+          if (snapshot.hasData &&
+              snapshot.data != null &&
+              (snapshot.data!).snapshot.value != null) {
+            final firebaseMessages = Map<dynamic, dynamic>.from(
+                (snapshot.data!).snapshot.value as Map<dynamic, dynamic>);
+            firebaseMessages.forEach((key, value) {
+              messageList.add(Message(
+                  text: value["text"],
+                  userId: value["userId"],
+                  timestamp: value["timestamp"]));
+            });
+            messageList.sort((a, b) => a.timestamp.compareTo(b.timestamp));
+
+            return Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: ListView.builder(
+                    reverse: false,
+                    itemCount: messageList.length,
+                    itemBuilder: (context, index) {
+                      return Padding(
+                        padding: const EdgeInsets.only(top: 16),
+                        child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Row(children: [
+                                Text(messageList[index].userId,
+                                    style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        color: Color(StringToHex.toColor(
+                                            messageList[index].userId)))),
+                                const SizedBox(width: 6),
+                                Text(
+                                  timeago.format(
+                                      DateTime.fromMillisecondsSinceEpoch(
+                                          messageList[index].timestamp)),
+                                  style: const TextStyle(
+                                      fontWeight: FontWeight.w400,
+                                      color: Colors.black38,
+                                      fontSize: 13.0),
+                                )
+                              ]),
+                              const SizedBox(
+                                height: 8,
+                              ),
+                              Text(messageList[index].text,
+                                  style: const TextStyle(fontSize: 16.0))
+                            ]),
+                      );
+                    }));
+          } else {
+            return const Center(child: Text("No messages"));
+          }
+        });
+  }
+}
+
+class BottomSheet extends StatelessWidget {
+  final TextEditingController _controller = TextEditingController();
+
+  BottomSheet({
+    super.key,
+    required DatabaseService database,
+  }) : _database = database;
+
+  @override
+  void dispose() {
+    _controller.dispose();
+  }
+
+  final DatabaseService _database;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20.0),
+      child: Row(
+        children: [
+          Expanded(
+            child: TextField(
+              controller: _controller,
+              style: const TextStyle(fontSize: 16.0),
+              decoration: const InputDecoration(
+                  labelText: "Message",
+                  border: OutlineInputBorder(),
+                  floatingLabelBehavior: FloatingLabelBehavior.never),
+            ),
+          ),
+          IconButton(
+              onPressed: () {
+                _database.sendMessage(_controller.text);
+                _controller.text = "";
+              },
+              icon: const Icon(Icons.send))
+        ],
+      ),
+    );
   }
 }
--- messenger/lib/models/message.dart
+++ messenger/lib/models/message.dart
@@ -1,20 +1,21 @@
-﻿class Message {
-  final String userId;
-  final String text;
-  final int timestamp;
+﻿import 'package:freezed_annotation/freezed_annotation.dart';
+// import 'package:flutter/foundation.dart';
 
-  Message({required this.userId, required this.text, required this.timestamp});
+part 'message.freezed.dart';
+part 'message.g.dart';
 
-  Message.fromJson(Map<String, dynamic> json)
-      : userId = json['iserId'],
-        text = json['text'],
-        timestamp = json['timestamp'];
+@freezed
+class Message with _$Message {
+  const factory Message(
+      {required String userId,
+      required final String text,
+      required final int timestamp}) = _Message;
 
-  Map<String, dynamic> toJson() =>
-      {'userId': userId, 'text': text, 'timestamp': timestamp};
+  factory Message.fromJson(Map<String, Object?> json) =>
+      _$MessageFromJson(json);
 
-  Message.fromMap(Map<String, dynamic> data)
-      : userId = data["userId"],
-        text = data['text'],
-        timestamp = data['timestamp'];
+  // Message.fromMap(Map<String, dynamic> data)
+  //     : userId = data["userId"],
+  //       text = data['text'],
+  //       timestamp = data['timestamp'];
 }

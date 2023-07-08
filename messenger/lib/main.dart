6cee369e7824455db211feacf59665f11f5ad1ed
YarikineZ <38810461+YarikineZ@users.noreply.github.com>
2023-07-08 17:29:03 +0300

    Вынес ALL в HomePage

--- messenger/lib/main.dart
+++ messenger/lib/main.dart
@@ -1,17 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 
-import 'package:messenger/pages/chats_list_page.dart';
-import 'package:messenger/pages/contacts_page.dart';
-import 'package:messenger/pages/settings_page.dart';
-
 import 'package:messenger/services/database_servise.dart';
 import 'package:messenger/services/shared_preferences_service.dart';
 import 'package:messenger/services/storage_servise.dart';
 
 import 'firebase_options.dart';
-
 import 'package:get_it/get_it.dart';
+import 'pages/home_page.dart';
 
 GetIt getIt = GetIt.instance;
 
@@ -48,57 +44,7 @@ class MyApp extends StatelessWidget {
         fontFamily: 'TimesNewRoman',
         useMaterial3: true,
       ),
-      home: const ALL(),
-    );
-  }
-}
-
-class ALL extends StatefulWidget {
-  const ALL({super.key});
-
-  @override
-  State<ALL> createState() => _ALLState();
-}
-
-class _ALLState extends State<ALL> {
-  int _selectedIndex = 0;
-  static const List<Widget> _widgetOptions = <Widget>[
-    ContactsPage(),
-    ChatsPage(),
-    SettingsPage(),
-  ];
-
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: _widgetOptions.elementAt(_selectedIndex),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.contacts),
-            label: 'Contacts',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.chat),
-            label: 'Chats',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.settings),
-            label: 'Settings',
-          ),
-        ],
-        currentIndex: _selectedIndex,
-        selectedItemColor: Colors.amber[800],
-        onTap: _onItemTapped,
-      ),
+      home: const HomePage(),
     );
   }
 }
---
+++ messenger/lib/pages/home_page.dart
@@ -0,0 +1,55 @@
+﻿import 'package:flutter/material.dart';
+import 'package:messenger/pages/settings_page.dart';
+
+import 'chats_list_page.dart';
+import 'contacts_page.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _selectedIndex = 0;
+  static const List<Widget> _widgetOptions = <Widget>[
+    ContactsPage(),
+    ChatsPage(),
+    SettingsPage(),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.contacts),
+            label: 'Contacts',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.chat),
+            label: 'Chats',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: 'Settings',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.amber[800],
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+}
